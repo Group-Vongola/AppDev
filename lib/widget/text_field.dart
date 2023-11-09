1733c5fdc6ad3dde5b44897c3f8bebf0748ae671
Lai Kai Chian <laichian@graduate.utm.my>
2023-11-09 21:18:51 +0800

    auth

---
+++ lib/widget/text_field.dart
@@ -0,0 +1,21 @@
+// import 'package:flutter/material.dart';
+
+// class NewTextField extends StatelessWidget {
+
+//   const NewTextField({
+     
+//     required String hintTextt,
+//     required bool obsecureTex,
+//     });
+//   @override
+//   Widget build(BuildContext context) {
+//     return TextFormField(
+//       obscureText: ,
+//       enableSuggestions: false,
+//       autocorrect: false,
+//       decoration: const InputDecoration(
+//         hintText: hintTextt,
+//       ),
+//     );
+//   }
+// }
